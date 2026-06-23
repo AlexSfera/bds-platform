9f05de6e70b992af8d9878af3015e4e6fde055a3
AlexSfera <alex@syncrosfera.com>
2026-06-23 17:41:10 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -5,6 +5,7 @@ const ALLOWED_IPS = [
   '77.208.169.57',  // Teléfono 1
   '77.208.160.208', // Teléfono 2
   '45.153.97.234',  // Casa
+  '90.167.42.33',   // [POR DEFINIR]
 ];
 
 export default function middleware(req) {
